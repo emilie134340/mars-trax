fd23cebaa129d65d4caa5772a6307b7af813a9a3
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-10-25 21:14:14 -0600

    added form, started js

---
+++ src/js/apodService.js
@@ -0,0 +1,10 @@
+// const baseUrl = "https://api.nasa.gov/planetary/apod?api_key=";
+// const apiKey = import.meta.env.API_KEY;
+// export async function getApodData() {}
+//     const response = await fetch(`${baseUrl}${apiKey}`);
+
+//     if (!response.ok) throw new Error("Failed to fetch data");
+
+//     const data = await response.json();
+//     return { url: data.url, title: data.title };
+
